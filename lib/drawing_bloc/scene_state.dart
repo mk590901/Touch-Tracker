593f6e74272c1364e7b820ca6601c18e7a0321a0
michael-k <michaelsure9@gmail.com>
2024-09-30 08:28:39 +0300

    Refactoring

--- lib/drawing_bloc/scene_state.dart
+++ lib/drawing_bloc/scene_state.dart
@@ -7,20 +7,15 @@ class SceneState extends Equatable {
   final List<Offset?> points;
   final Color color;
   final double lineWidth;
-  //final double zoomLevel;
-  //final bool gridMode;
-  //final Pair<int,int> offset;
-  //final int gridsNumber;
   final int sequentialNumber;
   final int gestureType;
   final Taps taps;
   final Taps longPresses;
 
   const SceneState({required this.points, required this.color,
-    required this.lineWidth, /*required this.zoomLevel, required this.gridMode,
-    required this.offset, required this.gridsNumber,*/ required this.sequentialNumber,
+    required this.lineWidth, required this.sequentialNumber,
     required this.gestureType, required this.taps, required this.longPresses});
 
   @override
-  List<Object?> get props => [points, color, lineWidth, /*zoomLevel, gridMode, offset, gridsNumber,*/ sequentialNumber, gestureType, taps, longPresses];
+  List<Object?> get props => [points, color, lineWidth, sequentialNumber, gestureType, taps, longPresses];
 }
