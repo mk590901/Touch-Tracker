7aae6b18e543bf198d393083c176f4d992a24280
michael-k <michaelsure9@gmail.com>
2024-09-30 11:55:35 +0300

    Refactoring

--- lib/drawing_bloc/drawing_bloc.dart
+++ lib/drawing_bloc/drawing_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../gesture/gesture_type.dart';
-import '../helpers/pair.dart';
 import '../helpers/tap.dart';
 import '../helpers/taps.dart';
 import 'drawing_events.dart';
@@ -12,32 +11,24 @@ class DrawingBloc extends Bloc<DrawingEvent, SceneState> {
   late int seqNumber = 0;
 
   DrawingBloc() : super (SceneState(points: const <Offset?>[],
-      color: Colors.lightBlue, lineWidth: 1.0,
-      /*zoomLevel: 1,
-      gridMode:  false,
-      offset:    Pair<int,int>(0,0),
-      gridsNumber: 10,*/ sequentialNumber: 0, gestureType: 0,
+      color: Colors.lightBlue, lineWidth: 1.0, sequentialNumber: 0, gestureType: 0,
       taps: Taps(), longPresses: Taps(),
       )) {
 
     on<StartDrawing>((event, emit) {
       final updatedPoints = List<Offset?>.from(state.points)
-        ..add(event.point/* / state.zoomLevel*/);
-      //putData('points', updatedPoints);
+        ..add(event.point);
       emit(SceneState(points: updatedPoints, color: state.color, lineWidth: state.lineWidth,
-      /*zoomLevel: state.zoomLevel, gridMode: state.gridMode, offset: state.offset,
-        gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: seqNumber++,
+        gestureType: state.gestureType, sequentialNumber: seqNumber++,
         taps: state.taps, longPresses: state.longPresses,
       ));
     });
 
     on<AddPoint>((event, emit) {
-      //  Need pay attention on event.point / state.zoomLevel + or maybe - state.offset
-      final updatedPoints = List<Offset?>.from(state.points)..add(event.point /*/ state.zoomLevel*/);
-      //putData('points', updatedPoints);
+      //  Need pay attention on event.point
+      final updatedPoints = List<Offset?>.from(state.points)..add(event.point);
       emit(SceneState(points: updatedPoints, color: state.color, lineWidth: state.lineWidth,
-      /*zoomLevel: state.zoomLevel, gridMode: state.gridMode, offset: state.offset,
-        gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: seqNumber++,
+        gestureType: state.gestureType, sequentialNumber: seqNumber++,
         taps: state.taps, longPresses: state.longPresses,
       ));
     });
@@ -45,61 +36,36 @@ class DrawingBloc extends Bloc<DrawingEvent, SceneState> {
     on<FinalDrawing>((event, emit) {
       seqNumber = 0;
       final updatedPoints = List<Offset?>.from(state.points)..add(null);
-      //putData('points', updatedPoints);
       emit(SceneState(points: updatedPoints, color: state.color, lineWidth: state.lineWidth,
-      /*zoomLevel: state.zoomLevel, gridMode: state.gridMode, offset: state.offset,
-        gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: seqNumber,
+        gestureType: state.gestureType, sequentialNumber: seqNumber,
         taps: state.taps, longPresses: state.longPresses,
       ));
     });
 
     on<ClearDrawing>((event, emit) {
-      //putData('points', null);
       emit(SceneState(points: const [], color: state.color, lineWidth: state.lineWidth,
-      /*zoomLevel: state.zoomLevel, gridMode: state.gridMode, offset: state.offset,
-        gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: state.sequentialNumber,
+        gestureType: state.gestureType, sequentialNumber: state.sequentialNumber,
         taps: state.taps, longPresses: state.longPresses,
       ));
     });
 
     on<ChangeColor>((event, emit) {
       emit(SceneState(points: state.points, color: event.color, lineWidth: state.lineWidth,
-      /*zoomLevel: state.zoomLevel, gridMode: state.gridMode, offset: state.offset,
-        gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: state.sequentialNumber,
+        gestureType: state.gestureType, sequentialNumber: state.sequentialNumber,
         taps: state.taps, longPresses: state.longPresses,
       ));
     });
 
     on<ChangeLineWidth>((event, emit) {
       emit(SceneState(points: state.points, color: state.color, lineWidth: event.lineWidth,
-      /*zoomLevel: state.zoomLevel, gridMode: state.gridMode, offset: state.offset,
-        gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: state.sequentialNumber,
+        gestureType: state.gestureType, sequentialNumber: state.sequentialNumber,
         taps: state.taps, longPresses: state.longPresses,
       ));
     });
 
-    // on<ChangeGridMode>((event, emit) {
-    //   //debugPrint('on<ChangeGridMode> [${event.gridMode}]');
-    //   //putData('grids', event.gridMode);
-    //   emit(SceneState(points: state.points, color: state.color, lineWidth: state.lineWidth,
-    //   /*zoomLevel: state.zoomLevel, gridMode: event.gridMode, offset: state.offset,
-    //     gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: state.sequentialNumber,
-    //     taps: state.taps, longPresses: state.longPresses,
-    //   ));
-    // });
-    //
-    // on<ChangeZoom>((event, emit) {
-    //   emit(SceneState(points: state.points, color: state.color, lineWidth: state.lineWidth,
-    //       /*zoomLevel: event.zoomLevel, gridMode: state.gridMode, offset: state.offset,
-    //     gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: state.sequentialNumber,
-    //     taps: state.taps, longPresses: state.longPresses,
-    //   ));
-    // });
-
     on<Refresh>((event, emit) {
       emit(SceneState(points: state.points, color: state.color, lineWidth: state.lineWidth,
-        /*zoomLevel: state.zoomLevel, gridMode: state.gridMode, offset: state.offset,
-        gridsNumber: state.gridsNumber,*/ gestureType: state.gestureType, sequentialNumber: seqNumber++,
+        gestureType: state.gestureType, sequentialNumber: seqNumber++,
         taps: state.taps, longPresses: state.longPresses,
       ));
     });
@@ -133,10 +99,6 @@ class DrawingBloc extends Bloc<DrawingEvent, SceneState> {
         points: state.points,
         color: state.color,
         lineWidth: state.lineWidth,
-        // zoomLevel: state.zoomLevel,
-        // gridMode: state.gridMode,
-        // offset: state.offset,
-        // gridsNumber: state.gridsNumber,
         gestureType: currentGestureType,
         sequentialNumber: seqNumber++,
         taps: (event.gestureType == GestureType.TAP_.index)
