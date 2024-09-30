d709251aa3e5cb7943779341cfef18fd92a4ef84
michael-k <michaelsure9@gmail.com>
2024-09-30 08:32:56 +0300

    Refactoring

--- lib/q_interfaces/i_gesture_listener.dart
+++ lib/q_interfaces/i_gesture_listener.dart
@@ -8,25 +8,4 @@ abstract class IGestureListener {
   void onLongPress(int pointer, Point<double> point);
   void onMove(int pointer, ActionModifier actionModifier, Point<double> point);
   void onPause(int pointer, Point<double> point);
-
-  // void onScroll(
-  //     int pointer,
-  //     ActionModifier actionModifier,
-  //     List<Point<double>>? downPoints,
-  //     List<Point<double>>? lastPoints,
-  //     Offset offset);
-  //
-  // void onZoom(
-  //     int pointer,
-  //     ActionModifier actionModifier,
-  //     List<Point<double>>? downPoints,
-  //     List<Point<double>>? lastPoints,
-  //     double parameter);
-  //
-  // void onRotate(
-  //     int pointer,
-  //     ActionModifier actionModifier,
-  //     List<Point<double>>? downPoints,
-  //     List<Point<double>>? lastPoints,
-  //     double? parameter);
 }
