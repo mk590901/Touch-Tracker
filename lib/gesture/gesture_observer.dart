987dd2249f976cc518b6d7c9852d0ea995006667
michael-k <michaelsure9@gmail.com>
2024-09-30 08:31:14 +0300

    Refactoring

--- lib/gesture/gesture_observer.dart
+++ lib/gesture/gesture_observer.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'dart:ui';
 import '../q_interfaces/i_gesture_listener.dart';
 import '../q_interfaces/i_update.dart';
 import 'gesture_type.dart';
@@ -25,25 +24,25 @@ class GestureObserver implements IGestureListener {
     _state?.update(GestureType.PAUSE_);
   }
 
-  @override
-  void onRotate(int pointer, ActionModifier actionModifier, List<Point<double>>? downPoints, List<Point<double>>? lastPoints, double? parameter) {
-    _state?.update(actionModifier == ActionModifier.Final ? GestureType.NONE_ : GestureType.ROTATION_, parameter);
-  }
-
-  @override
-  void onScroll(int pointer, ActionModifier actionModifier, List<Point<double>>? downPoints, List<Point<double>>? lastPoints, Offset offset) {
-    _state?.update(actionModifier == ActionModifier.Final ? GestureType.NONE_ : GestureType.SCROLL_, offset);
-    _state?.updateScroll(actionModifier, offset);
-  }
+  // @override
+  // void onRotate(int pointer, ActionModifier actionModifier, List<Point<double>>? downPoints, List<Point<double>>? lastPoints, double? parameter) {
+  //   _state?.update(actionModifier == ActionModifier.Final ? GestureType.NONE_ : GestureType.ROTATION_, parameter);
+  // }
+  //
+  // @override
+  // void onScroll(int pointer, ActionModifier actionModifier, List<Point<double>>? downPoints, List<Point<double>>? lastPoints, Offset offset) {
+  //   _state?.update(actionModifier == ActionModifier.Final ? GestureType.NONE_ : GestureType.SCROLL_, offset);
+  //   _state?.updateScroll(actionModifier, offset);
+  // }
 
   @override
   void onTap(int pointer, Point<double> point) {
     _state?.updateAndReset(GestureType.TAP_, point);
   }
 
-  @override
-  void onZoom(int pointer, ActionModifier actionModifier, List<Point<double>>? downPoints, List<Point<double>>? lastPoints, double parameter) {
-    _state?.update(actionModifier == ActionModifier.Final ? GestureType.NONE_ : GestureType.ZOOM_, parameter);
-    _state?.updateZoom(actionModifier, parameter);
-  }
+  // @override
+  // void onZoom(int pointer, ActionModifier actionModifier, List<Point<double>>? downPoints, List<Point<double>>? lastPoints, double parameter) {
+  //   _state?.update(actionModifier == ActionModifier.Final ? GestureType.NONE_ : GestureType.ZOOM_, parameter);
+  //   _state?.updateZoom(actionModifier, parameter);
+  // }
 }
--- lib/q_interfaces/i_gesture_listener.dart
+++ lib/q_interfaces/i_gesture_listener.dart
@@ -9,24 +9,24 @@ abstract class IGestureListener {
   void onMove(int pointer, ActionModifier actionModifier, Point<double> point);
   void onPause(int pointer, Point<double> point);
 
-  void onScroll(
-      int pointer,
-      ActionModifier actionModifier,
-      List<Point<double>>? downPoints,
-      List<Point<double>>? lastPoints,
-      Offset offset);
-
-  void onZoom(
-      int pointer,
-      ActionModifier actionModifier,
-      List<Point<double>>? downPoints,
-      List<Point<double>>? lastPoints,
-      double parameter);
-
-  void onRotate(
-      int pointer,
-      ActionModifier actionModifier,
-      List<Point<double>>? downPoints,
-      List<Point<double>>? lastPoints,
-      double? parameter);
+  // void onScroll(
+  //     int pointer,
+  //     ActionModifier actionModifier,
+  //     List<Point<double>>? downPoints,
+  //     List<Point<double>>? lastPoints,
+  //     Offset offset);
+  //
+  // void onZoom(
+  //     int pointer,
+  //     ActionModifier actionModifier,
+  //     List<Point<double>>? downPoints,
+  //     List<Point<double>>? lastPoints,
+  //     double parameter);
+  //
+  // void onRotate(
+  //     int pointer,
+  //     ActionModifier actionModifier,
+  //     List<Point<double>>? downPoints,
+  //     List<Point<double>>? lastPoints,
+  //     double? parameter);
 }
